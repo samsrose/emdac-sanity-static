e1dbf66001d1d8885f6b9ac407a81b97d8e33145
Sam Rose <samrosegamgee@gmail.com>
2024-03-07 19:05:01 -0800

    added vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./styles/global.css";
 import Meta from "../components/Meta";
 import Navbar from "../components/Navbar";
+import { Analytics } from "@vercel/analytics/react"
 
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Meta />
+      <Analytics />
       
       <Navbar bgColor="bg-gray-800" />
     
