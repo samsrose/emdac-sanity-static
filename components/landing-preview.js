9e77f918a0fa7bd436434b264cca80229ed338d7
samsrose <samrosegamgee@gmail.com>
2026-06-04 13:39:26 -0700

    spacing updates

--- components/landing-preview.js
+++
@@ -1,8 +0,0 @@
-import { usePreview } from '../lib/sanity'
-import { indexQuery } from '../lib/queries'
-import Landing from './landing'
-
-export default function LandingPreview({ allPosts }) {
-  const previewAllPosts = usePreview(indexQuery)
-  return <Landing allPosts={previewAllPosts ?? allPosts} preview />
-}
